3b73ccfc32ad2294a8ad8bc724fec7425500ed1e
DhanushPadarthi <dhanushpadarthi@example.com>
2026-04-15 16:53:09 +0530

    Remove openid scope for GS3B339AE9 - issued docs + profile only

--- backend/src/controllers/digilockerController.js
+++ backend/src/controllers/digilockerController.js
@@ -55,7 +55,6 @@ exports.getAuthUrl = async (req, res, next) => {
       state,
       code_challenge: codeChallenge,
       code_challenge_method: 'S256',
-      scope: 'openid',
     });
 
     const authUrl = `${DL_BASE}/public/oauth2/1/authorize?${params.toString()}`;
